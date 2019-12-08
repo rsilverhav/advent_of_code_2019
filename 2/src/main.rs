69f0cc331856d87cedebff8af4a48a0be64a335c
Robin <robinsilverhav@gmail.com>
2019-12-08 15:24:53 +0100

    parser works, should refactor param extraction to function, added test cases

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,28 +1,49 @@
-fn opcode_parse(opcodes: Vec<i32>) -> Vec<i32> {
-    for opcode in opcodes.chunks(4) {
-        let code = &opcode[0];
-        match code {
+fn opcode_parse(mut data: Vec<i32>) -> Vec<i32> {
+    let mut i = 0;
+    while i < data.len() {
+        let opcode = data[i];
+        match opcode {
             1 => {
-                let params = &opcode[1..4];
-                println!("should add, params: {:?}", params);
+                let val_a_index = data[i + 1] as usize;
+                let val_b_index = data[i + 2] as usize;
+                let to = data[i + 3] as usize;
+                data[to] = data[val_a_index] + data[val_b_index];
+                i += 3
             }
             2 => {
-                let params = &opcode[1..4];
-                println!("should multi, params {:?}", params);
+                let val_a_index = data[i + 1] as usize;
+                let val_b_index = data[i + 2] as usize;
+                let to = data[i + 3] as usize;
+                data[to] = data[val_a_index] * data[val_b_index];
+                i += 3
             }
             99 => {
-                println!("EXIT PLEASE");
                 break;
             }
-            _ => println!("Unknown opcode: {}", code),
+            _ => i += 1,
         }
     }
-    return opcodes;
+
+    return data;
 }
 
 fn main() {
-    let input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    assert_eq!(
+        &opcode_parse(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])[..],
+        &[3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+    );
 
-    let test = opcode_parse(input);
-    println!("test: {:?}", test);
+    assert_eq!(&opcode_parse(vec![1, 0, 0, 0, 99])[..], &[2, 0, 0, 0, 99]);
+
+    assert_eq!(&opcode_parse(vec![2, 3, 0, 3, 99])[..], &[2, 3, 0, 6, 99]);
+
+    assert_eq!(
+        &opcode_parse(vec![2, 4, 4, 5, 99, 0])[..],
+        &[2, 4, 4, 5, 99, 9801]
+    );
+
+    assert_eq!(
+        &opcode_parse(vec![1, 1, 1, 4, 99, 5, 6, 0, 99])[..],
+        &[30, 1, 1, 4, 2, 5, 6, 0, 99]
+    );
 }
